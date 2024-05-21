331f665599e22f7533e29811e81903190c9e9e58
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-05-21 19:58:52 +0300

    init map

---
+++ src/main/java/org/demo/springpostgis/controller/MapController.java
@@ -0,0 +1,12 @@
+package org.demo.springpostgis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+    @GetMapping("/map")
+    public String map() {
+        return "pages/map";
+    }
+}
